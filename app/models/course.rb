988c94f1b8b92ebfd05090adf9ab757678e8b432
Giordano Alves <giordano@gridfinnance.ie>
2024-05-27 17:23:57 +0100

    Initial setup of project.
    * replace sqlit for portgres(already installed locally);
    * add some gems for development and test;
    * add github actions

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  description   :string
+#  thumbnail_url :string
+#  title         :string
+#  video_url     :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -11,13 +13,13 @@
 Course.find_or_create_by!(
   title: "Ocean fishing",
   description: "Learn how to dive like a majestic bird.",
-  thumbnail_url: 'https://source.unsplash.com/flying-bird-over-body-of-water-MPYUwhE8Jjs',
-  video_url: '//vjs.zencdn.net/v/oceans.mp4'
+  thumbnail_url: "https://source.unsplash.com/flying-bird-over-body-of-water-MPYUwhE8Jjs",
+  video_url: "//vjs.zencdn.net/v/oceans.mp4"
 )
 
 Course.find_or_create_by!(
   title: "Driving a car",
   description: "Learn how to drive a car like a pro.",
-  thumbnail_url: 'https://source.unsplash.com/car-on-road-near-mountain-during-daytime-a_5DTTj_zm4',
-  video_url: '//archive.org/download/CEP217/CEP217_512kb.mp4'
+  thumbnail_url: "https://source.unsplash.com/car-on-road-near-mountain-during-daytime-a_5DTTj_zm4",
+  video_url: "//archive.org/download/CEP217/CEP217_512kb.mp4"
 )
